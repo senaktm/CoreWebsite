0dd6c4c75de813410c93d61b82f0c5449093a742
senaktm <senaoktm@gmail.com>
2024-03-20 01:09:42 +0300

    Portfolio added

---
+++ DataAccessLayer/Migrations/20240319214537_mig_add_portfoliocolumns.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_add_portfoliocolumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Completion",
+                table: "Portfolios",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl3",
+                table: "Portfolios",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl4",
+                table: "Portfolios",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Platform",
+                table: "Portfolios",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Price",
+                table: "Portfolios",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Status",
+                table: "Portfolios",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Completion",
+                table: "Portfolios");
+
+            migrationBuilder.DropColumn(
+                name: "ImageUrl3",
+                table: "Portfolios");
+
+            migrationBuilder.DropColumn(
+                name: "ImageUrl4",
+                table: "Portfolios");
+
+            migrationBuilder.DropColumn(
+                name: "Platform",
+                table: "Portfolios");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Portfolios");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Portfolios");
+        }
+    }
+}
