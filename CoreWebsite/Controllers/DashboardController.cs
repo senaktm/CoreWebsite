cf9005d0f84fd5556842b1ff5190e3e0b5cabc3c
senaktm <senaoktm@gmail.com>
2024-03-20 16:31:26 +0300

    Arrangements were made according to the new theme.

---
+++ CoreWebsite/Controllers/DashboardController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreWebsite.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "Dashboard Page";
+            ViewBag.v2 = "Dashboard";
+            ViewBag.v3 = "EditDashboard";
+            return View();
+        }
+    }
+}
