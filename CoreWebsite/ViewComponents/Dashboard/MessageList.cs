fa2e9e4c83e7bf179dd2ad10f3325c34a36b79c5
senaktm <senaoktm@gmail.com>
2024-03-21 03:55:22 +0300

    ViewComponents Updated

---
+++ CoreWebsite/ViewComponents/Dashboard/MessageList.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreWebsite.ViewComponents.Dashboard
+{
+    public class MessageList : ViewComponent
+    {
+        UserMessageManager userMessageManager = new UserMessageManager(new EfUserMessageDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = userMessageManager.GetUserMessageWithUserService();
+            return View(values);
+        }
+    }
+}
---
+++ CoreWebsite/ViewComponents/Portfolio/SlideList.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace CoreWebsite.ViewComponents.Portfolio
+{
+    public class SlideListViewComponent : ViewComponent
+    {
+        PortfolioManager portfolioManager = new PortfolioManager(new EfPortfolioDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = portfolioManager.TGetList();
+            return View(values);
+        }
+    }
+}
