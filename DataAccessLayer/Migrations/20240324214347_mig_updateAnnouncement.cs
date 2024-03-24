68efef390a87a2dbdb3c0c4eee83f506c197ebeb
senaktm <senaoktm@gmail.com>
2024-03-25 00:49:01 +0300

    migration re-updated for Announcement entity

---
+++ DataAccessLayer/Migrations/20240324214347_mig_updateAnnouncement.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_updateAnnouncement : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Announcements",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Announcements");
+        }
+    }
+}
