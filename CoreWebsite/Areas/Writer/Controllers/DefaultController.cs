4552411ad0ac81014a124a0022c5414da5c9acb0
senaktm <senaoktm@gmail.com>
2024-03-25 01:27:22 +0300

    Announcement Page added

--- CoreWebsite/Areas/Writer/Controllers/DefaultController.cs
+++ CoreWebsite/Areas/Writer/Controllers/DefaultController.cs
@@ -1,13 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreWebsite.Areas.Writer.Controllers
-{
+{   
+    [Area("Writer")]
+    [Authorize]
     public class DefaultController : Controller
     {
-        [Area("Writer")]
+        AnnouncementManager announcementManager = new AnnouncementManager(new EfAnnouncementDal());
         public IActionResult Index()
         {
-            return View();
+            var values = announcementManager.TGetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AnnouncementDetails(int id)
+        {
+            var values = announcementManager.TGetById(id);
+            return View(values);
         }
     }
 }
