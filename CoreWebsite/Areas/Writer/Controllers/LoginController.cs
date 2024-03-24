bf78bf79d9cadb69373dea91a06867202cfe59d4
senaktm <senaoktm@gmail.com>
2024-03-25 00:23:37 +0300

    Login Page added

---
+++ CoreWebsite/Areas/Writer/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using CoreWebsite.Areas.Writer.Models;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreWebsite.Areas.Writer.Controllers
+{
+    [Area("Writer")]
+    public class LoginController : Controller
+    {
+        private readonly SignInManager<WriterUser> _signInManager;
+
+        public LoginController(SignInManager<WriterUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(UserLoginViewModel p)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(p.Username,p.Password,true,true);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index","Default");
+                }
+                else
+                {
+                    ModelState.AddModelError("","Incorrect username or password ");
+                }
+            }
+            return View();
+        }
+    }
+}
