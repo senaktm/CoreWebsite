813aa229ecd61e9d051bb2d01ecb15046588193e
senaktm <senaoktm@gmail.com>
2024-03-25 00:24:04 +0300

    LoginViewModel added

---
+++ CoreWebsite/Areas/Writer/Models/UserLoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoreWebsite.Areas.Writer.Models
+{
+    public class UserLoginViewModel
+    {
+        [Display(Name ="Username")]
+        [Required(ErrorMessage ="Please enter username")]
+        public string Username  { get; set; }
+
+        [Display(Name = "Password")]
+        [Required(ErrorMessage = "Please enter password")]
+        public string Password { get; set; }
+    }
+}
