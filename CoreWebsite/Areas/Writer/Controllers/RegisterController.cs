d661976db04ed849c8a56c89f2ef3bbb67a264e9
senaktm <senaoktm@gmail.com>
2024-03-25 00:24:46 +0300

    Register Page added

---
+++ CoreWebsite/Areas/Writer/Controllers/RegisterController.cs
@@ -0,0 +1,60 @@
+﻿using BusinessLayer.Concrete;
+using CoreWebsite.Areas.Writer.Models;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreWebsite.Areas.Writer.Controllers
+{
+    [Area("Writer")]
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<WriterUser> userManager;
+
+        public RegisterController(UserManager<WriterUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new UserRegisterViewModel());
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(UserRegisterViewModel p)
+        {
+            if (ModelState.IsValid)
+            {
+                WriterUser writerUser = new WriterUser()
+                {
+                    Name = p.Name,
+                    Surname = p.Surname,
+                    Email = p.Email,
+                    UserName = p.UserName,
+                    ImageUrl = p.ImageUrl
+                };
+                if (p.Password == p.ConfirmPassword)
+                {
+
+
+                    var result = await userManager.CreateAsync(writerUser, p.Password);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Login");
+                    }
+                    else
+                    {
+                        foreach (var item in result.Errors)
+                        {
+                            ModelState.AddModelError("", item.Description);
+                        }
+                    }
+                }
+            }
+
+            return View();
+        }
+    }
+}
