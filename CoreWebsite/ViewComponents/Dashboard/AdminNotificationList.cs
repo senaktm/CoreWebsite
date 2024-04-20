23446543545275be9612a770c9add1c4f28a1876
senaktm <senaoktm@gmail.com>
2024-04-21 00:47:31 +0300

    Navbar Notification updated

---
+++ CoreWebsite/ViewComponents/Dashboard/AdminNotificationList.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+using System.Runtime.Intrinsics.X86;
+
+namespace CoreWebsite.ViewComponents.Dashboard
+{
+    public class AdminNotificationList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
