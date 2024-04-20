09ede5767841801d5051c6035031d9b2b2994aee
senaktm <senaoktm@gmail.com>
2024-04-20 21:57:57 +0300

    SubContent added

---
+++ CoreWebsite/Controllers/SubContentController.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreWebsite.Controllers
+{
+    public class SubContentController : Controller
+    {
+        ContactManager contactManager = new ContactManager(new EfContactDal());
+        [HttpGet]
+        public IActionResult Index()
+        {
+
+            var values = contactManager.TGetById(1);
+            return View(values);
+        }
+
+        [HttpPost]
+        public IActionResult Index(Contact p)
+        {
+            contactManager.TUpdate(p);
+            return RedirectToAction("Index", "Default");
+        }
+    }
+}
