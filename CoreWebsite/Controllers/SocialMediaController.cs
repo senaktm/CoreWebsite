693c750d5ad65c0775bcc44206443746d956e8d9
senaktm <senaoktm@gmail.com>
2024-04-20 23:48:20 +0300

    SocialMedia added

---
+++ CoreWebsite/Controllers/SocialMediaController.cs
@@ -0,0 +1,51 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreWebsite.Controllers
+{
+    public class SocialMediaController : Controller
+    {
+        SocialMediaManager socialMediaManager = new SocialMediaManager(new EfSocialMediaDal());
+        public IActionResult Index()
+        {
+            var values = socialMediaManager.TGetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult DeleteSocialMedia(int id)
+        {
+            var value = socialMediaManager.TGetById(id);
+            socialMediaManager.TDelete(value);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult EditSocialMedia(int id)
+        {
+            var value = socialMediaManager.TGetById(id);
+            return View(value);
+        }
+        [HttpPost]
+        public IActionResult EditSocialMedia(SocialMedia p)
+        {
+            p.Icon = " ";
+            p.Status = true;
+            socialMediaManager.TUpdate(p);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult AddSocialMedia()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddSocialMedia(SocialMedia p)
+        {
+            p.Status = true;
+            p.Icon = " ";
+            socialMediaManager.TAdd(p);
+            return RedirectToAction("Index");
+        }
+    }
+}
