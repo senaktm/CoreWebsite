e455c51fb4bf7c901fad3e849c73910d09203129
senaktm <senaoktm@gmail.com>
2024-04-21 00:47:15 +0300

    NavbarMessageList updated

---
+++ CoreWebsite/ViewComponents/Dashboard/AdminNavbarMessageList.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreWebsite.ViewComponents.Dashboard
+{
+    public class AdminNavbarMessageList : ViewComponent
+    {
+        WriterMessageManager writerMessageManager = new WriterMessageManager(new EfWriterMessageDal());
+        public IViewComponentResult Invoke()
+        {
+            string p = "admin@gmail.com";
+            var value = writerMessageManager.GetListReceiverMessage(p).OrderByDescending(x=>x.WriterMessageID).Take(3).ToList();
+            return View(value);
+        }
+    }
+}
